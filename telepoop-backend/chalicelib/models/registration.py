f8373ebed74cc642061eb0d3252cc85c5cce8a7f
goudhabuddha <2770287+goudhabuddha@users.noreply.github.com>
2022-03-31 16:45:11 -0700

    fixed function name

--- telepoop-backend/chalicelib/models/registration.py
+++ telepoop-backend/chalicelib/models/registration.py
@@ -12,7 +12,7 @@ class RegistrationDetail(BaseModel):
             raise ValidationError(f'Username must be greater than {min_length} characters.')
     
     @validator('password')
-    def name_min_length(cls, v: str):
+    def password_min_length(cls, v: str):
         min_length = 8
         if len(v) < min_length:
             raise ValidationError(f'Password must be greater than {min_length} characters.')
